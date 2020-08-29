0616a1aec3c59cc1fd77129f281f0b774338257a
Usman <usman_9@outlook.com>
2020-08-30 02:16:30 +0500

    Rendering html page with if conditions

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def greetings():
-    headLine = "Usman is freaking awesome!"
-    return render_template("index.html", headLine = headLine)
+    today = datetime.datetime.now()
+    new_year = today.month == 1 and today.day==1
+    return render_template("index.html", new_year = new_year)
