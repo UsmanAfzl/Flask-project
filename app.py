e6954c238b1d3b9cca41b6a7858425d373bb1f79
Usman <usman_9@outlook.com>
2020-08-30 02:30:34 +0500

    Redering html page with for loop

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
-import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def greetings():
-    today = datetime.datetime.now()
-    new_year = today.month == 1 and today.day==1
-    return render_template("index.html", new_year = new_year)
+    names = ["Alice", "Jacob","Billie"]
+    return render_template("index.html", names = names)
